1af6085e5f851465bb694c10818df572c4a2d8b5
Simon <simon.nicholas@outlook.com>
2026-04-22 14:02:04 +1000

    Final Cart fix

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -5,8 +5,12 @@ import { FunctionContext } from '../App.js';
 import './SignUpPage.css';
 
 function SignUpPage() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
   const [message, setMessage] = useState('');
   const navigate = useNavigate();
   const { handleLoading, setUser } = useContext(FunctionContext);
@@ -36,9 +40,34 @@ function SignUpPage() {
     <div className="SignUpPage">
       <form className="sign-up-form" onSubmit={handleSignup}>
         <input
+          className="first-name"
+          type="text"
+          placeholder="First Name"
+          value={firstName}
+          onChange={e => setFirstName(e.target.value)}
+          required
+        />
+        <input
+          className="last-name"
+          type="text"
+          placeholder="Last Name"
+          value={lastName}
+          onChange={e => setLastName(e.target.value)}
+          required
+        />
+        <input
+          className="phone-number"
+          type="tel"
+          placeholder="Phone Number"
+          value={phoneNumber}
+          onChange={e => setPhoneNumber(e.target.value)}
+          required
+        />
+        <input
           className="sign-up-email"
           type="email"
           placeholder="Email"
+          autoComplete="off"
           value={email}
           onChange={e => setEmail(e.target.value)}
           required
@@ -46,8 +75,17 @@ function SignUpPage() {
         <input
           className="sign-up-password"
           type="password"
+          autoComplete="new-password"
           placeholder="Password"
           value={password}
+          onChange={e => setPassword(e.target.value)}
+          required
+        />
+        <input
+          className="repeat-password"
+          type="password"
+          placeholder="Repeat Password"
+          value={repeatPassword}
           onChange={e => setPassword(e.target.value)}
           required
         />
